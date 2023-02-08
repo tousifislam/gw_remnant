7bc9106b957505772caadb98d62f0b58d2b36478
Tousif Islam <tislam@rps2.cscvr>
2023-02-08 00:51:12 -0500

    adding utility for peak kick velocity

--- gw_remnant/remnant_calculators/kick_velocity_calculator.py
+++ gw_remnant/remnant_calculators/kick_velocity_calculator.py
@@ -32,6 +32,7 @@ class LinearMomentumCalculator(RemnantMassCalculator):
         self.voft = self._compute_voft()
         self.kickoft = self._compute_kickoft()
         self.remnant_kick = self._compute_remnant_kick()
+        self.peak_kick = self. _compute_peak_kick()
     
     def _read_dhdt_dict(self, l, m):
         """
@@ -143,6 +144,33 @@ class LinearMomentumCalculator(RemnantMassCalculator):
         """
         return np.array([np.linalg.norm(self.voft[i]) for i in range(len(self.time))])
 
+    def _get_peak_via_quadratic_fit(self, t, func):
+        """
+        Finds the peak time of a function quadratically
+        Fits the function to a quadratic over the 5 points closest to the argmax func.
+        t : an array of times
+        func : array of function values
+        Returns: tpeak, fpeak
+        """
+        # Find the time closest to the peak, making sure we have room on either side
+        index = np.argmax(func)
+        index = max(2, min(len(t) - 3, index))
+        # Do a quadratic fit to 5 points,
+        # subtracting t[index] to make the matrix inversion nice
+        testTimes = t[index-2:index+3] - t[index]
+        testFuncs = func[index-2:index+3]
+        xVecs = np.array([np.ones(5),testTimes,testTimes**2.])
+        invMat = np.linalg.inv(np.array([[v1.dot(v2) for v1 in xVecs] \
+            for v2 in xVecs]))
+        yVec = np.array([testFuncs.dot(v1) for v1 in xVecs])
+        coefs = np.array([yVec.dot(v1) for v1 in invMat])
+        return t[index] - coefs[1]/(2.*coefs[2]), coefs[0] - coefs[1]**2./4/coefs[2]
+    
+    def _compute_peak_kick(self):
+        """
+        Computes the peak of kick velocity profile
+        """
+        return self._get_peak_via_quadratic_fit(self.time, self.kickoft)[1]
     
     def _compute_remnant_kick(self):
         """
