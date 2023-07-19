275daf51e36ff3a58fde08719fcd255de6fcbd63
tousifislam <tousifislam24@gmail.com>
2023-07-19 00:40:05 -0400

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 long_description = open('README.md').read()
 
 setup(name='gw_remnant',
-      version='0.0.2',
+      version='0.0.1',
       author='Tousif Islam, Scott Field, Gaurav Khanna',
       author_email='tousifislam24@gmail.com',
       #packages=['BHPTNRremnant'],
